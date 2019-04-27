55d2d30dc88b3e2a679f66fb7d414fc1290f0d91
namratasiv <43491187+namratasiv@users.noreply.github.com>
2019-04-27 11:39:01 -0500

    Add files via upload

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,60 +1,80 @@
 import React from "react";
+import { Textbox, Textarea, Radiobox, Checkbox, Select } from 'react-inputs-validation';
+import 'react-inputs-validation/lib/react-inputs-validation.min.css';
+import UserAvatar from "react-user-avatar";
+import { extname } from "path";
+import "../exam.css";
+import {commitMutation, graphql} from "react-relay";
+import environment from "../../environment";
 
 export default class Settings extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            username: '',
+            fname: ''
+        }
+        this.emailChange = this.emailChange.bind(this);
+        this.nameChange = this.nameChange.bind(this);
+        this.submit = this.submit.bind(this);
+    }
+    emailChange(event){
+        this.setState({username: event.target.value});
+    }
+    nameChange(event){
+        this.setState({fname: event.target.value});
+    }
+    submit(){
+        const variables = {
+            username: this.state.username,
+            fname: this.state.fname
+        }
+        
+    }
     render(){
         return (
-            <div class="container-fluid" style={{marginTop: "40px"}}>
-                <div class="row" style={{height: "250px"}}>
+            <div className="container-fluid" style={{marginTop: "10px"}}>
+                <div className="row" style={{height: "250px"}}>
                     <div  style={{margin: "0 auto"}}>
                         <div style={{textAlign: "center"}}>
-                            <img src="https://media.licdn.com/dms/image/C5603AQHULsiaPLHOxg/profile-displayphoto-shrink_200_200/0?e=1560988800&v=beta&t=PhkPRL6yTdzDBUO0m1ewgYUIag72elbKMeQPJkXwkmM" width="100px" height="100px" style={{borderRadius: "100px"}}/>
-                            <h5 style={{marginTop: "10px"}}>VigneashSundar</h5>
-                            <h6 style={{color: "#00c730"}}>$vikene</h6>
+
+                            
+                            
+                             <UserAvatar size="100" name={localStorage.getItem('Username')} colors={['whitesmoke']} className="UserAvatar--inner"/>
+                             <h6 style={{color: "#00c730"}}>{localStorage.getItem('Username')}</h6>
+                             <h6 style={{color: "#00c730"}}>{localStorage.getItem("Fname")}</h6>                           
+
                         </div>
                     </div>  
                 </div>
                 <hr />
-                <div class="row" >
-                    <div class="col-md-12" style={{textAlign: "center"}}>
-                        <p style={{color:"#999",fontSize: "22px"}}>Display Name</p>
-                        <div class="card " style={{width: "18rem",margin: "0 auto"}}>
-                        <div class="card-body">
-                            <div class="row"  style={{width: "100%",margin: "0 auto", textAlign: "center"}}>
-                                <h5>Vigneash Sundar</h5>
-                                
-                            </div>
-                        </div>
-                        </div>
-                        <p style={{color:"#999",fontSize: "22px", marginTop: "20px"}}>Phone</p>
-                        <div class="card " style={{width: "18rem",margin: "0 auto"}}>
-                        <div class="card-body">
-                            <div class="row"  style={{width: "100%",margin: "0 auto", textAlign: "center"}}>
-                                <h5>(469) 270-1938</h5>
-                            </div>
-                        </div>
-                        </div>   
-                        <p style={{color:"#999",fontSize: "22px", marginTop: "20px"}}>Change PIN</p>
-                        <div class="card " style={{width: "18rem",margin: "0 auto"}}>
-                        <div class="card-body">
-                            <div class="row"  style={{width: "100%",margin: "0 auto", textAlign: "center"}}>
-                                <h5>****</h5>
-                            </div>
-                        </div>
-                        </div>
-                        <p style={{color:"#999",fontSize: "22px", marginTop: "20px"}}>Profile Description</p>
-                        <div class="card " style={{width: "18rem",margin: "0 auto"}}>
-                        <div class="card-body">
-                            <div class="row"  style={{width: "100%",margin: "0 auto", textAlign: "center"}}>
-                                <p>This is me GUYS!</p>
-                            </div>
-                        </div>
-                        </div>
-                        <button class="btn btn-primary" style={{margin: "20px"}}>Update</button>
-                    </div>
+                <div className="form-update">
+                <div className="text-center mb-4">
+                <h1 className = "h3 mb-3 font-weight-normal">Update your Information</h1>
+                
                 </div>
-            
-            
-            </div>
+
+                <div className="form-label-group">
+                        <input type="name" id="inputfname" className="form-control" placeholder="First Name" required autofocus value={this.state.fname} onChange={this.onfnameChange} />
+                        <label for="inputfname">First Name</label>
+                    </div>
+                    <div className="form-label-group">
+                            <input type="name" id="inputlname" className="form-control" placeholder="Last Name" required autofocus value={this.state.lname} onChange={this.onlnameChange} />
+                            <label for="inputlname">Last Name</label>
+                    </div>
+                    
+                <div className="form-label-group">
+                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus  value={this.state.email} onChange={this.onusernameChange}/>
+                    <label for="inputEmail">Email address</label>
+                </div>
+                <button className="btn btn-lg btn-primary btn-block"  onClick={this.submit}>Update!</button>
+                <p className="mt-5 mb-3 text-muted text-center">© 2019 SecurePay</p>
+                </div>
+                
+                </div>
+                
         )
     }
+    
 }
